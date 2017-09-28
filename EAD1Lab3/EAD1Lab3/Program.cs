75c773cac75bba788c7aa35b064909235054ea19
klambe17@gmail.com <klambe17@gmail.com>
2017-09-28 16:48:06 +0100

    added polymorphic array to test class

--- EAD1Lab3/EAD1Lab3/Program.cs
+++ EAD1Lab3/EAD1Lab3/Program.cs
@@ -10,35 +10,28 @@ namespace EAD1Lab3
     {
         static void Main(string[] args)
         {
+
             Sphere t1 = new Sphere();
             Sphere t2 = new Sphere("Sphere", 25);
 
+            EAD1Lab3.IHasVolume[] collection;
+            collection = new IHasVolume[5];
+
+            collection[0] = t1;
+            collection[1] = t2;
+
+
             Console.WriteLine(t1.ToString());
             Console.WriteLine(t2.ToString());
+
+            Console.WriteLine("************** Array Printout ************");
+
+            foreach (IHasVolume s in collection)
+            {
+                Console.WriteLine(s + " volume: " + s.calculateVolume());
+            }
+
             Console.ReadLine();
         }
     }
 }
-
-
-//public int Id
-//{
-//    set
-//    {
-//        if (value >= 0)
-//        {
-//            _id = value;
-//        }
-
-//    }
-//    get
-//    {
-//        return this._id;
-
-//    }
-//}
-//public string Email
-//{
-//    get;
-//    set;
-//}
