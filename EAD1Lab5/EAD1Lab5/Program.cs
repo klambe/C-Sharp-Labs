d8d51d9009797a95927f23d38b522d26e1cba742
klambe17@gmail.com <klambe17@gmail.com>
2017-10-02 20:11:01 +0100

    added struct and enum for lab 5

--- EAD1Lab5/EAD1Lab5/Program.cs
+++ EAD1Lab5/EAD1Lab5/Program.cs
@@ -8,8 +8,87 @@ namespace EAD1Lab5
 {
     class Program
     {
+        public struct Money
+        {
+            //public double euroPerDollar;
+            //public double yenPerDollar;
+            //public double yenPerEuro;
+            public Currency CurrencyType { get; set; }
+            public double TotalAmount { get; set; }
+
+            public Money(Currency currencyIn, double amountIn)
+            {
+                CurrencyType = currencyIn;
+                TotalAmount = amountIn;
+            }
+
+            public double convertCurrency(Currency newCurrency)
+            {
+               double  yenPerEuro = 0.0076;
+               double  euroPerDollar = 1.17;
+               double  yenPerDollar = 0.0089;
+
+                    if(CurrencyType == Currency.Yen && newCurrency ==Currency.Us)
+                    {
+                        TotalAmount = TotalAmount * yenPerDollar;                       
+                    }
+                    else if (CurrencyType == Currency.Yen && newCurrency == Currency.Euro)
+                    {
+                        TotalAmount = TotalAmount * yenPerEuro;
+                    }
+                    else if (CurrencyType == Currency.Euro && newCurrency == Currency.Us)
+                    {
+                        TotalAmount = TotalAmount * euroPerDollar;
+                    }
+                    else if (CurrencyType == Currency.Euro && newCurrency == Currency.Yen)
+                    {
+                        TotalAmount = TotalAmount / yenPerEuro;
+                    }
+                    else if (CurrencyType == Currency.Us && newCurrency == Currency.Euro)
+                    {
+                        TotalAmount = TotalAmount / euroPerDollar;
+                    }
+                    else if (CurrencyType == Currency.Us && newCurrency == Currency.Yen)
+                    {
+                        TotalAmount = TotalAmount / yenPerDollar;
+                    }                
+
+                CurrencyType = newCurrency;
+                return TotalAmount;
+
+            }
+        }
+        public enum Currency { Euro, Yen, Us }
+
+        public Money addTwoCurrencies(Money a, Money b)
+        {
+            b.convertCurrency(a.CurrencyType);
+
+            double sumOfCurrency = a.TotalAmount + b.TotalAmount;
+            Money result = new Money(a.CurrencyType, sumOfCurrency);
+            return result;
+        }
+
         static void Main(string[] args)
         {
+
+             Money addTwoCurrencies(Money a, Money b)
+        {
+            b.convertCurrency(a.CurrencyType);
+
+            double sumOfCurrency = a.TotalAmount + b.TotalAmount;
+            Money result = new Money(a.CurrencyType, sumOfCurrency);
+            return result;
+        }
+            Currency firstCurrency = Currency.Euro;
+            Currency secondCurrency = Currency.Us;
+
+            Money firstMoney = new Money(firstCurrency, 1000);
+            Money secondMoney = new Money(secondCurrency, 1000);
+
+            Money convertMoneyAndAddTotal = addTwoCurrencies(firstMoney, secondMoney);
+
+            Console.WriteLine("Total when converted to euro is: " + convertMoneyAndAddTotal.TotalAmount);
         }
     }
 }
